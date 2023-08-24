226a02b4a6c46f262d453115b234b1e637373121
justin <justinmothupi@gmail.com>
2023-08-24 17:31:11 +0200

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,6 @@
  *
  * Retun: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,6 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("Size of a char:%d byte(s)\n",sizeof(char));
